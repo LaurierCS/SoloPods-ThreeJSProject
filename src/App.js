b209acc00afad35e4e87bd76c9585f511e94562d
ObiTracks <straightup.stack@gmail.com>
2022-05-12 19:17:52 -0400

    Created structure for selecting the textures and position data for each planet depending on its name

--- src/App.js
+++ src/App.js
@@ -1,34 +1,87 @@
 import React, {useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-import {Canvas, useLoader} from '@react-three/fiber';
+import {Canvas, useLoader, useFrame} from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import { OrbitControls, Stars, useTexture } from '@react-three/drei';
 
 import './App.css';
 
-function Planet() {
-  const props = useTexture({
-    map: "Mars_Map.jpg",
+function Planet(props) {
+
+  // const textures = useTexture({
+  //   map: props.map,
+  // });
+  const textures = useTexture(props.details.textures);
+
+  const myMesh = React.useRef();
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    myMesh.current.rotation.y = a/40;
   });
+
   return (
-    <mesh scale={1}>
+    <mesh ref={myMesh} {...props.details.meshProperties}>
+
+    {/* <mesh ref={myMesh} position={[1,2,3]}> */}
       <sphereBufferGeometry attach = "geometry"/>
-      <meshStandardMaterial attach="material" {...props}/>
+      <meshStandardMaterial attach="material" {...textures}/>
       {/* <meshLambertMaterial attach="material" color="hotpink"/> */}
     </mesh>
   )
 }
 
+
+const planetProperties = { 
+  mercury: {
+    textures:{
+      map: "Mercury_Map.jpg",
+    },
+    meshProperties: {
+      position: [0, 0, 0]
+    }
+  },
+  venus: {
+    textures:{
+      map: "Venus_Map.jpg",
+    },
+    meshProperties: {
+      position: [3, 0, 0]
+    }
+  },
+  mars: {
+    textures:{
+      map: "Mars_Map.jpg",
+    },
+    meshProperties: {
+      position: [6, 0, 0]
+    }
+  },
+  earth: {
+    textures:{
+      map: "Earth_Map.jpg",
+    },
+    meshProperties: {
+      position: [9, 0, 0]
+    }
+  },
+};
+
 function App() {
+  
   return (
     <Canvas>
-      <OrbitControls autoRotate autoRotateSpeed={0.6}/>
+      <OrbitControls autoRotate autoRotateSpeed={0.05}/>
       <ambientLight intensity={0.5}/>
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-
+      <spotLight position={[10, 15, 10]} angle={3} intensity={2} />
       <Stars />
-      <Planet />
+      <Planet details={planetProperties.mercury} />
+      <Planet details={planetProperties.venus} />
+      <Planet details={planetProperties.mars} />
+      <Planet details={planetProperties.earth} />
+      {/* <Planet />
+      <Planet /> */}
     </Canvas>
   );
 }
