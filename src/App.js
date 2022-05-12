a95562c51944cee162cc91ebd10d28fc032d11c1
ObiTracks <straightup.stack@gmail.com>
2022-05-12 10:17:50 -0400

    Started the app and got basic sphere with some lighting added to the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, {useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import {Canvas, useLoader} from '@react-three/fiber';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { OrbitControls, Stars } from '@react-three/drei';
+
 import './App.css';
+
+function Planet() {
+  // const textureMap = useLoader(TextureLoader, "./images/Mars_Map.jpg");
+  return (
+    <mesh scale={1}>
+      <sphereBufferGeometry attach = "geometry"/>
+      <meshStandardMaterial attach="material" />
+      {/* <meshLambertMaterial attach="material" color="hotpink"/> */}
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls/>
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+
+      <Stars />
+      <Planet />
+    </Canvas>
   );
 }
 
